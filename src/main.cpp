6c984c98fc9f5d97252c472a3e62e3f7e1818bf0
Usva Pellinen <julius.pellinen@gmail.com>
2026-03-17 16:50:26 +0200

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 int main() {
 	std::cout << "hello world\n";
 	return 0;
+// Connect to UNIX-domain stream socket
+// nc -U /tmp/testsocket
+// irssi
+// /connect 127.0.0.1 6767
+
 struct clientDetails {
     int32_t          clientfd;
     int32_t          serverfd;
