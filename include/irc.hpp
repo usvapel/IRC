d3a22ff14df494ea94dc3984aec6216dc76e134c
Usva Pellinen <julius.pellinen@gmail.com>
2026-03-17 16:45:49 +0200

    header

--- include/irc.hpp
+++ include/irc.hpp
@@ -1,1 +1,29 @@
+#include <asm-generic/socket.h>
+#include <err.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <cstdlib>
 #include <iostream>
+#include <vector>
+
+/*
+ * htonl, htons, ntohl, ntohs
+ * - convert values between host and network byte order
+ */
+#include <arpa/inet.h>
+
+/*
+ * getprotobyname, getprotobynumber, getprotent — network protocol
+ * database functions
+ */
+#include <netdb.h>
+
+#define MY_SOCK_PATH "/tmp/testsocket"
+#define LISTEN_BACKLOG 50
