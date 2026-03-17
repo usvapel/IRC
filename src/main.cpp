92b285d2441c3b8902601054c8721f5fa58b118a
Usva Pellinen <julius.pellinen@gmail.com>
2026-03-17 16:50:10 +0200

    simple struct for fds

--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,13 @@
 int main() {
 	std::cout << "hello world\n";
 	return 0;
+struct clientDetails {
+    int32_t          clientfd;
+    int32_t          serverfd;
+    std::vector<int> clientList;
+    clientDetails(void) {
+      this->clientfd = -1;
+      this->serverfd = -1;
+    }
+};
 }
